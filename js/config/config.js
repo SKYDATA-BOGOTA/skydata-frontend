d7bb5daca0b37b8f3cefedae0a04ee4742bc7105
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2025-11-29 08:58:59 -0500

    Ajuste de ip API

--- js/config/config.js
+++ js/config/config.js
@@ -9,4 +9,4 @@
 // SwR-I02: Cliente HTTP Frontend
 // Implementa requisito de interfaz SwR-I02 según ISO/IEC/IEEE 29148:2018
 // Configuración para despliegue en EC2 (IP Pública)
-window.API_URL = 'http://18.234.36.47:3000/api';
+window.API_URL = 'http://98.88.82.195:3000/api';
